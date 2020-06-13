a53b88b32db500b2013a560c0842ce82e63cedb4
Joshua Spence <joshua-spence@outlook.com>
2020-06-13 01:16:40 +0100

    New structured website

--- sandbox.js
+++ sandbox.js
@@ -1,70 +1,31 @@
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-//First code structure for horizontal scroll
-var firstImage = document.querySelector('.group1');
-var secImage = document.querySelector('.group2');
-var thirdImage = document.querySelector('.group3');
-
-//This function adjusts the gap between photos when the window screen is resized
-window.addEventListener("resize", () =>{
-
-    var imgGap = screen.width - (screen.width - window.innerWidth);
-    var widthResize = (screen.width -window.innerWidth) * -0.022;
-    var heightResize = (screen.height - window.innerHeight) * 0.02;
-
-    //Decreases or increases the size of the gap between each image bases on the width of the screen.
-    secImage.style.transform = `translateX(${imgGap}px)`;
-    thirdImage.style.transform = `translateX(${imgGap * 2}px)`;
-
-    //When shrinking the height of the viweport it keeps the image centered
-    firstImage.style.left = `${33.5 + heightResize}vw`;
-    secImage.style.left = `${33.5 + heightResize}vw`;
-    thirdImage.style.left = `${33.5 + heightResize}vw`;
+  loop: true,
 });
 
 
-gsap.from('.collection', {opacity: 1, duration: 1, delay: 1, y:'100vh'});
-gsap.from('span', {opacity: 0, duration: 3, delay: 2});
-
-
-function enterAnimation() {
-    const tl = gasp.timeline();
-    tl.to('.home', {   
-        opacity: 0,
-        duration: 1
-    });
-
-    tl.from('.project1', {
-        y: '100vh',
-        duration: 1
-    }, '-=0.3');
+gsap.from('.landing-title', {opacity:0, ease: 'power2', x: -100, delay: 1, duration: 1 });
+gsap.from('.swiper-project-slide', {opacity: 0, ease: 'power2', x: 80, delay: 0.5, duration: 1});
+gsap.from('.logo-top-left', {opacity:0, delay: 0.2, duration: 1, ease: 'power1' });
+gsap.from('.aboutMe-bottom-left', {opacity:0, delay: 0.2, duration: 1.5, ease: 'power1' });
+gsap.from('.contactMe-bottom-right', {opacity:0, delay: 0.2, duration: 1.5, ease: 'power1' });
 
 
 
-}
+$(".swiper-button-prev").click(function() {
 
-barba.init({
-    transitions: [{
-        enter(){
-            enterAnimation() 
-        }
-    }]
+  gsap.from('.landing-title', {x: -20, duration: 1});
+  gsap.from('.swiper-project-slide', {x: -20, duration: 1});
+
 });
 
+$(".swiper-button-next").click(function() {
 
+  gsap.from('.landing-title', {x: 20, duration: 1});
+  gsap.from('.swiper-project-slide', {x: 20, duration: 1});
 
-
-// Transitions through the photos 
-// var myIndex = 0;
-// carousel();
-
-// function carousel(){
-//     var i;
-//     var x = document.getElementsByClassName("photos");
-//     for(i = 0; i < x.length; i++){
-//         x[i].style.display = "none";
-//     }
-//     myIndex++;
-//     if(myIndex > x.length) {myIndex = 1}
-//     x[myIndex-1].style.display = "block";
-//     setTimeout(carousel, 900);    
-// }
+});
