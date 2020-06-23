6c84f05626b65acdbe8aae0c6d7612700e73ef40
Joshua Spence <joshua-spence@outlook.com>
2020-06-24 00:14:24 +0100

    completed

--- landing.js
+++ landing.js
@@ -33,8 +33,8 @@ if(localStorage.getItem('activeIndex')){
 //when page loads the gsap animations will be executed 
 window.onload = function(){
   gsap.from('.landing-title', {autoAlpha: 0, x:-50, duration:0.5, delay:1});
-  gsap.from('.landing-photo-link', {autoAlpha:0, y:200, duration:1});
-  gsap.from('.logo-top-left', {y:100, duration:1});
+  gsap.from('.landing-photo-link', {autoAlpha:0, y:200, duration:1, delay: 0.2});
+  gsap.from('.logo-top-left', {y:50, duration:1.2});
   gsap.from('.portfolio-bottom-left', {opacity:0, y:20, duration:1});
   gsap.from('.contactMe-bottom-right', {opacity:0, y:20, duration:1});
 };
